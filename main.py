a3c7dd41908683e7e8abe9b7ea2be1d5b3b21d02
SweatyBytes <maxvheiningen@gmail.com>
2024-10-02 22:58:43 +0200

    open_book and count_words added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+def main():
+    book_contents = open_book() # run the count_words function, based on the output of open_book
+    word_count = count_words(book_contents)  # Store the result
+    print(f"Frankenstein book contains {word_count} words!")
+
+def open_book():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def count_words(f):
+    words = len(f.split())
+    return words
+
+# run main
+main()
